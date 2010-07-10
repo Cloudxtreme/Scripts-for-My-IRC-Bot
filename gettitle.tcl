62443fbd405a641cfa60bead09de5a50c6d414a6
Angel <me@angelbroz.com>
2010-07-10 15:46:38 -0500

    Fix for Shorten Url

--- gettitle.tcl
+++ gettitle.tcl
@@ -1,19 +1,15 @@
 ######################### --- ABURL Stuffs --- ###############################
 # 1.- When an user says a URL in the channel, the bot says the Title of the URL
-# 2.- Saying the command .surl <url>, the bot says the bit.ly shorted url
+# 2.- Saying the command !surl <url>, the bot says the bit.ly shorted url
 #
 #             Angel Gonzalez <me@angelbroz.com>
 
 package require http
 set urlpattern {(http|https)://[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]}
 
-#your api here
-set bitlyapi {R_7a0d1c3d308b4c789f1549c634319272d} 
-#your but.ly user here
-set bitlylogin {angelbroz} 
-
+set bitlyapi {R_7a0d1c3d308b4b77f1549c634319272d}
+set bitlylogin {angelbroz}
 set bitlyurl {http://api.bit.ly/v3/shorten?login=}
-set bitlyurl [concat $bitlyurl $bitlylogin "&apikey=" $bitlyapi "&longUrl="]
 
 bind pubm - * checkurl
 
@@ -35,17 +31,17 @@ proc gettitle {urls} {
         putlog "no title $urls"
 }
 
-#Look for an URL in the text, if match returns the URL
+Look for an URL in the text, if match returns the URL
 proc isurl {text} {
         global  urlpattern
         
         if {[regexp -nocase -- $urlpattern $text match]} {
                 return $match
-        }
+        }                                               
         return 0
 }  
 
-#This procedure is called everytime that the users says somth in the channel
+# This procedure is called everytime that the users says somth in the channel
 proc checkurl {nick uhost hand channel text} {
         putlog $text
         set myurl [isurl $text]
@@ -55,26 +51,26 @@ proc checkurl {nick uhost hand channel text} {
                 putlog "sending priv"
                 putserv  "PRIVMSG $channel :$title"
         }
-}                          
-                
+}                                                                                        
+
 bind pub - .surl shorturl
-                
+bind pub - !surl shorturl
+
 proc shorturl {nick uhost hand channel text} {
-        global bitlyurl
+        global bitlyurl bitlylogin bitlyapi
+
+        set apicall [concat $bitlyurl $bitlylogin "&apikey=" $bitlyapi "&longUrl="]
         set myurl [isurl $text]
         if {[expr {$myurl eq 0} ? false : true]} {
-                set urls [http::formatQuery $myurl]   
-                set bitlyurl [concat $bitlyurl $urls "&format=txt"]
-                set bitlyurl [string map {" " ""} $bitlyurl]
-                set token [http::geturl $bitlyurl]
+                set urls [http::formatQuery $myurl]
+                set apicall [concat $apicall $urls "&format=txt"]
+                set apicall [string map {" " ""} $apicall]
+                putlog "$apicall"
+                set token [http::geturl $apicall]
                 set data  [http::data $token]
-                
-                putserv  "PRIVMSG $channel :$data"
+
+                putserv  "PRIVMSG $channel :$nick -> $data"
         }
-}  
+}
 
 
-
-        
-        
-        
